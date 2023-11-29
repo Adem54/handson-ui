dc90f0b29ea589dcc88f36fa7a69699b383ba764
Adem <adem5434e@gmail.com>
2023-11-30 00:17:08 +0100

    update

--- app/main-page5.ios.ts
+++ app/main-page5.ios.ts
@@ -4,16 +4,9 @@ export function onLoaded(args:EventData)
 {
   let label = args.object as Label;
 
-  //Platform a gore nasil check yapabiliriz...
-  if(platformNames.android)
-  {
-    label.backgroundColor = new Color("#a9a9a9");
-    label.backgroundColor = new Color("pink");
-    label.color = new Color("#000");
-  }else if(platformNames.ios)
-  {
     label.borderColor = new Color("tomato");
+    label.color = new Color("#ffff");
 
   }
 
-}
+
--- app/main-page5.ts
+++ app/main-page5.ts
@@ -1,4 +1,6 @@
-import { Color, EventData, Label, platformNames } from "@nativescript/core";
+import { Application, Color, EventData, Label, platformNames } from "@nativescript/core";
+import * as app from '@nativescript/core/application'
+
 
 export function onLoaded(args:EventData)
 {
@@ -7,13 +9,24 @@ export function onLoaded(args:EventData)
   //Platform a gore nasil check yapabiliriz...
   if(platformNames.android)
   {
-    label.backgroundColor = new Color("#a9a9a9");
-    label.backgroundColor = new Color("pink");
-    label.color = new Color("#000");
-  }else if(platformNames.ios)
+    // label.backgroundColor = new Color("#a9a9a9");
+    // label.backgroundColor = new Color("pink");
+    // label.color = new Color("#000");
+
+
+      //BU ESKI SURUME GORE BOYLE YAPILMIS AMA ARTIK GECERLI DEGIL...muhtemelen app yerine Application i kullanmak gerek ve gerisi de artik ona gore dokumandan bulunup ayarlanabilir...BUNMLARI IYI OGRENMEK GEREK
+      //BU OTOMATIK OLARAK, STATUSBAR I DARK OLMAYA ZORLAYACAK
+      const window = app.android.startActivity.getWindow();
+      const decorView = window.getDecorView();
+      decorView.setSystemUiVisibility(android.view.View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+      //App_Resources altindaki Android/src/res/values/colors.xml de ise    <color name="ns_primaryDark">#fefefe</color> kismin coor ini seklinde degistiririz...ve bu sekilde arka plan beyyyaz.....color yazi rengi ise dark olmus olacaktir androidde!!!!!
+
+
+    }else if(platformNames.ios)
   {
     label.borderColor = new Color("tomato");
 
   }
+
 
 }
